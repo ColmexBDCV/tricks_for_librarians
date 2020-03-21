0ccd60f503db4bd925e07564a6bf44a94ed21a2d
Silvia Gutiérrez <silviaegt@gmail.com>
2020-03-20 18:41:24 -0600

    :gift: añadí sugerencia de TeresaOM <3

--- RCode/obtener_numerodevacios_porcolumna.R
+++ RCode/obtener_numerodevacios_porcolumna.R
@@ -5,3 +5,6 @@ reporte_df <- mi_df %>%
   as_tibble(rownames = NA) #este parámetro es para que no ignore los nombres de fila, ¡gracias @josschavezf!
 
 write.csv(reporte_df, "reporte_df.csv")
+#Vale la pena explorar: https://cran.r-project.org/web/packages/DataExplorer/vignettes/dataexplorer-intro.html
+#un paquete para ver faltantes y echarle ojo rápido a los datos vía @TeresaOM
+         
