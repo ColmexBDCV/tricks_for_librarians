9848c4f2b440770219a036af487f846ed700145a
Silvia Gutiérrez <silviaegt@gmail.com>
2020-05-27 21:59:53 -0500

    código para juntar csvs

---
+++ RCode/analisis_citas/juntar_multiples_csvs_patron-nombre.R
@@ -0,0 +1,111 @@
+###### Código para juntar múltiples CSVs con un patrón en el nombre 
+### notas de cómo juntar múltiples documentos: https://www.masterdataanalysis.com/r/working-with-files-and-folders-in-r/
+
+library(readr)
+library(plyr)
+
+CEAA <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^CEAA(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+
+CEH <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^CEH(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+
+CEI <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^CEI(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+
+CELL <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^CELL(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+
+PEI <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^PEI(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+
+
+CES <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^CES(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+
+CEDUA <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^CEDUA(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+CEE <- plyr::rbind.fill(
+  lapply(
+    list.files(
+      "2015_2019", 
+      pattern = "^CEE(.*)csv$", 
+      full.names = TRUE
+    ), 
+    read_csv
+  )
+)
+
+CEH$Centro <- "CEH"
+CEAA$Centro <- "CEAA"
+CEI$Centro <-"CEI"
+CELL$Centro <- "CELL" 
+PEI$Centro <- "PEI"
+CES$Centro <- "CES"
+CEDUA$Centro <- "CEDUA"
+CEE$Centro <- "CEE"
+
+citantes <- rbind.fill(PEI, CES, CEDUA, CEE, CEH, CEAA, CEI, CELL)
