86898c9dc1733bd9789fef6fc3fcf674360f69d8
Silvia Gutiérrez <silviaegt@gmail.com>
2021-02-01 12:54:47 -0600

    :wrench: mínimos arreglos
    algunos cambios para ayudar a la legibilidad de las instrucciones

--- RCode/pdftools-y-tesseract/pdftools-y-tesseract_demo.R
+++ RCode/pdftools-y-tesseract/pdftools-y-tesseract_demo.R
@@ -2,35 +2,34 @@
 # Autor: Silvia Gutiérrez (@espejolento)
 # Licencia: CC BY 4.0
 
+# En esta lección usaremos cuatro paquetes de R, si no los tienen instalados
+# pueden instalarlos con la función: instal.packages("nombre del paquete entrecomillado")
+
 library(pdftools)
 library(stringr)
 library(readr)
 library(tesseract)
 
-
-
 ############# Ejemplo 1: Texto nativo digital #################
 
 # Leemos el texto con la siguiente función de pdftools:
 ley <- pdftools::pdf_text("Ley_Federal_Refugiados.pdf")
-ley
+ley #siempre podemos ver qué hay en nuestra variable escribiendo su nombre y dando Ctrl+Enter (o bien desde nuestras variables de entorno)
 
 # La función guardó cada página en vectores distintos
 # para guardar todo el texto en la misma variable usamos la función de stringr, str_c() y el parámetro "collapse"
 ley <- stringr::str_c(ley, collapse = "")
-ley
+ley #ver línea 14
 
 # Guardarlo en un archivo de texto plano con la función de readr: write_lines() 
 readr::write_lines(ley, "ley.txt")
 
 ############# Ejemplo 2: Texto/imagen (escaneado) #################
 
-
 tesseract::tesseract_download("spa") # se bajan los datos de entrenamiento del idioma que nos interesa
 
 yanez <- pdftools::pdf_ocr_text("yanez_declaraciones.pdf", lang="spa") #se aplica OCR usando el idioma que bajamos
 
-
-yanez <- stringr::str_c(yanez, collapse = "")
+yanez <- stringr::str_c(yanez, collapse = "") #ver líneas 16-18
 
 readr::write_lines(yanez, "yanez.txt") #con la función write_lines podemos crear un documento de texto plano con el texto extraído con OCR
