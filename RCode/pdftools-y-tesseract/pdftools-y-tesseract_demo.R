b7cc2007bfa2894c102ef6d434fc206bd6b1fd6b
Silvia Gutiérrez <silviaegt@gmail.com>
2021-02-01 20:29:53 -0600

    :wrench: cambios para ser más precisa

--- RCode/pdftools-y-tesseract/pdftools-y-tesseract_demo.R
+++ RCode/pdftools-y-tesseract/pdftools-y-tesseract_demo.R
@@ -3,6 +3,7 @@
 # Licencia: CC BY 4.0
 # Inspirado en el taller con @rivaquiroga: https://github.com/silviaegt/RLadiesCDMX_talleres/blob/master/extraccion_pdfs/importacion-y-limpieza-textos.R
 
+# ¡ANTES! Fíjate bien que el pdf que quieres transformar esté en la misma carpeta que tu código
 # En esta lección usaremos cuatro paquetes de R, si no los tienen instalados
 # pueden instalarlos con la función: instal.packages("nombre del paquete entrecomillado")
 
@@ -19,11 +20,12 @@ ley #siempre podemos ver qué hay en nuestra variable escribiendo su nombre y da
 
 # La función guardó cada página en vectores distintos
 # para guardar todo el texto en la misma variable usamos la función de stringr, str_c() y el parámetro "collapse"
-ley <- stringr::str_c(ley, collapse = "")
-ley #ver línea 14
+ley_texto <- stringr::str_c(ley, collapse = "")
+ley_texto #podemos guardar el texto colapsado en una nueva variable, así puedes ver la diferencia
 
-# Guardarlo en un archivo de texto plano con la función de readr: write_lines() 
-readr::write_lines(ley, "ley.txt")
+# Guarda tu texto colapsado (el que generaste en la línea 23) 
+#en un archivo de texto plano con la función de readr: write_lines() 
+readr::write_lines(ley_texto, "ley.txt")
 
 ############# Ejemplo 2: Texto/imagen (escaneado) #################
 
@@ -31,6 +33,6 @@ tesseract::tesseract_download("spa") # se bajan los datos de entrenamiento del i
 
 yanez <- pdftools::pdf_ocr_text("yanez_declaraciones.pdf", lang="spa") #se aplica OCR usando el idioma que bajamos
 
-yanez <- stringr::str_c(yanez, collapse = "") #ver líneas 16-18
+yanez_texto <- stringr::str_c(yanez, collapse = "") #ver líneas 21-24 para entender qué hace esta función
 
-readr::write_lines(yanez, "yanez.txt") #con la función write_lines podemos crear un documento de texto plano con el texto extraído con OCR
+readr::write_lines(yanez_texto, "yanez.txt") #con la función write_lines podemos crear un documento de texto plano con el texto extraído con OCR
